85c6a6f8f6ed47c49421fcd0520cc097ae7081c7
Abhishek khilan Patel <abhishekpatel9370@gmail.com>
2025-09-25 13:29:17 +0530

    Create top.sv

---
+++ top.sv
@@ -0,0 +1,23 @@
+
+// Code your design here
+module dff
+  (
+    input clk, rst, din, ////din - data input, rst - active high synchronus
+    output reg dout ////dout - data output
+  );
+  
+  always@(posedge clk)
+    begin
+      if(rst == 1'b1) 
+        dout <= 1'b0;
+      else
+        dout <= din;
+    end
+  
+endmodule
+
+interface ff() ;
+  logic clk ,rst ,din ;
+  logic dout ;
+  
+endinterface 
